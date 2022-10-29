ea372f63d66c692242c75611cafcb16badccb716
StarMindz <64619873+StarMindz@users.noreply.github.com>
2022-10-29 16:10:39 +0100

    Add Components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,38 +2,38 @@ import React, { Component } from 'react';
 import Button from './Button';
 
 class Calculator extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: null};
-    }
-    render() {
-        return(
-        <div className="calculator">
-            <div className="display">0</div>
-            <div className="input"> 
-            <Button value='AC'/>
-            <Button value='+/-'/>
-            <Button value='%'/>
-            <Button value='+'/>
-            <Button value='7'/>
-            <Button value='8'/>
-            <Button value='9'/>
-            <Button value='x'/>
-            <Button value='4'/>
-            <Button value='5'/>
-            <Button value='6'/>
-            <Button value='-'/>
-            <Button value='1'/>
-            <Button value='2'/>
-            <Button value='3'/>
-            <Button value='+'/>
-            <Button value='0'/>
-            <Button value='.'/>
-            <Button value='='/>
-            </div>
+  constructor(props) {
+    super(props);
+    this.state = { value: null};
+  }
+  render() {
+    return (
+      <div className="calculator">
+        <div className="display">0</div>
+        <div className="input"> 
+          <Button value="AC" />
+          <Button value="+/-" />
+          <Button value="%" />
+          <Button value="+" />
+          <Button value="7" />
+          <Button value="8" />
+          <Button value="9" />
+          <Button value="x" />
+          <Button value="4" />
+          <Button value="5" />
+          <Button value="6" />
+          <Button value="-" />
+          <Button value="1" />
+          <Button value="2" />
+          <Button value="3" />
+          <Button value="+" />
+          <Button value="0" />
+          <Button value="." />
+          <Button value="=" />
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
 export default Calculator;
