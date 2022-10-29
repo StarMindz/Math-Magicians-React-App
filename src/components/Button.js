0203011002c18bb2850c446bba263c4224b3b104
StarMindz <64619873+StarMindz@users.noreply.github.com>
2022-10-29 14:07:38 +0100

    Add functionalities

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 
 class Button extends Component {
-    state = {operators: ['=', '-', '+', 'x', '/', '']};
+    
 
     constructor(props) {
         super(props)
+        this.state = {operators: ['=', '-', '+', 'x', '/', '']};
     }
 
     render() {
-    if (this.props.value === '0') {return <div className="double-button button">{this.props.value}</div>}
-    else {return <div className={this.state.operators.includes(this.props.value)? 'special-button':'button'}>{this.props.value}</div>}}
+    if (this.props.value === '0') {return <div onClick={() =>this.props.click(this.props.value)} className="double-button button">{this.props.value}</div>}
+    else {return <div onClick={() =>this.props.click(this.props.value)} className={this.state.operators.includes(this.props.value)? 'special-button':'button'}>{this.props.value}</div>}}
 }
 
 export default Button;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,16 +3,21 @@ import Button from "./Button";
 
 const buttonArr = ['AC', '+/-', '%', '/', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '=']
 class Calculator extends Component {
-    state = {screen: '0'}
+
+    constructor(props) {
+        super(props);
+        this.state = {screen: '0'};
+    }
 
     updateScreen(value) {
-        const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-        if (digits.includes(value.item))
-        {
-            this.setState(state => ({
-            screen: state.screen + value.item
-            }))
-        }
+        console.log(value);
+        console.log(this.state.screen);
+        // console.log(this.state);
+        // const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+        // if (digits.includes(value))
+        // {
+        //     this.setState({screen: this.state.screen + value});
+        // }
     }
 
     render() {
@@ -21,7 +26,8 @@ class Calculator extends Component {
             <div className="display">{this.state.screen}</div>
             <div className="input"> 
             {buttonArr.map((item, index) => {
-                return <div onClick={() =>this.updateScreen({item})}> <Button value={item}/> </div>
+                // <div onClick={() =>this.updateScreen({item})}>  </div>  
+                return <Button value={item} click={this.updateScreen}/>
             })}
             </div>
         </div>
