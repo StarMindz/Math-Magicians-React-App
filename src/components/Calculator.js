b2dd8570d697009487545ce446e4dde689c9bd4e
starmindz <64619873+StarMindz@users.noreply.github.com>
2022-10-30 05:24:20 +0100

    Refractor class to functions using hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,48 +1,42 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.updateScreen = this.updateScreen.bind(this);
+const Calculator = () => {
+  const [state, setState] = useState(0);
+
+  const updateScreen = (element) => {
+    setState((state) => calculate(state, element.target.innerText));
   }
 
-  updateScreen(element) {
-    this.setState((state) => calculate(state, element.target.innerText));
-  }
-
-  render() {
-    const { next, result } = this.state;
-    return (
-      <div className="calculator">
-        <div className="display">
-          <span>{next || result || 0}</span>
-        </div>
-        <div className="input">
-          <div className="button" onClick={this.updateScreen}>AC</div>
-          <div className="button" onClick={this.updateScreen}>+/-</div>
-          <div className="button" onClick={this.updateScreen}>%</div>
-          <div className="button special-button" onClick={this.updateScreen}>÷</div>
-          <div className="button" onClick={this.updateScreen}>7</div>
-          <div className="button" onClick={this.updateScreen}>8</div>
-          <div className="button" onClick={this.updateScreen}>9</div>
-          <div className="button special-button" onClick={this.updateScreen}>x</div>
-          <div className="button" onClick={this.updateScreen}>4</div>
-          <div className="button" onClick={this.updateScreen}>5</div>
-          <div className="button" onClick={this.updateScreen}>6</div>
-          <div className="button special-button" onClick={this.updateScreen}>-</div>
-          <div className="button" onClick={this.updateScreen}>1</div>
-          <div className="button" onClick={this.updateScreen}>2</div>
-          <div className="button" onClick={this.updateScreen}>3</div>
-          <div className="button special-button" onClick={this.updateScreen}>+</div>
-          <div className="button double-button" onClick={this.updateScreen}>0</div>
-          <div className="button" onClick={this.updateScreen}>.</div>
-          <div className="button special-button" onClick={this.updateScreen}>=</div>
-        </div>
+  return (
+    <div className="calculator">
+      <div className="display">
+        <span>{state.next || state.result || 0}</span>
       </div>
+      <div className="input">
+        <div className="button" onClick={updateScreen}>AC</div>
+        <div className="button" onClick={updateScreen}>+/-</div>
+        <div className="button" onClick={updateScreen}>%</div>
+        <div className="button special-button" onClick={updateScreen}>÷</div>
+        <div className="button" onClick={updateScreen}>7</div>
+        <div className="button" onClick={updateScreen}>8</div>
+        <div className="button" onClick={updateScreen}>9</div>
+        <div className="button special-button" onClick={updateScreen}>x</div>
+        <div className="button" onClick={updateScreen}>4</div>
+        <div className="button" onClick={updateScreen}>5</div>
+        <div className="button" onClick={updateScreen}>6</div>
+        <div className="button special-button" onClick={updateScreen}>-</div>
+        <div className="button" onClick={updateScreen}>1</div>
+        <div className="button" onClick={updateScreen}>2</div>
+        <div className="button" onClick={updateScreen}>3</div>
+        <div className="button special-button" onClick={updateScreen}>+</div>
+        <div className="button double-button" onClick={updateScreen}>0</div>
+        <div className="button" onClick={updateScreen}>.</div>
+        <div className="button special-button" onClick={updateScreen}>=</div>
+      </div>
+    </div>
     );
   }
 }
