3281b85765378a479a2893b485bf68e9b6a2ca94
starmindz <64619873+StarMindz@users.noreply.github.com>
2022-10-30 04:47:45 +0100

    Add component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,5 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions*/
-/* eslint-disable jsx-a11y/click-events-have-key-events*/
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { Component } from 'react';
 import calculate from '../logic/calculate';
 
