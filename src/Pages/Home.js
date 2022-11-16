165e2b2c84a7dd0b585fa3de24dabb6f89a0c24d
StarMindz <64619873+StarMindz@users.noreply.github.com>
2022-11-16 17:43:41 +0100

    Write good texts in Hom page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,7 +6,18 @@ const Home = () => {
       <div className="HomeInner">
         <h2>Welcome to our page Genius!</h2>
         <p className="HomeText">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ligula sem, vestibulum ut elit eget, ullamcorper ultricies enim. Sed gravida tortor condimentum condimentum ultricies. Sed ornare dui a justo faucibus, sit amet lobortis dui lacinia. Nunc porta volutpat ante eu tincidunt. Curabitur metus nisi, imperdiet eu scelerisque at, cursus at orci. Fusce non sollicitudin velit. Nunc ultrices quam nec suscipit rhoncus. Suspendisse potenti. Aenean sit amet egestas lectus. Fusce condimentum ligula id neque ultrices, at posuere tortor sodales. Nullam blandit mauris eu neque venenatis fermentum. Vestibulum aliquet ex eu rutrum dapibus. Curabitur efficitur, felis non pretium tincidunt, lacus neque tincidunt ipsum, nec mattis mauris urna non sem. Sed eu neque non odio pellentesque vehicula. Maecenas eget dapibus tortor. Vivamus blandit magna in eros placerat maximus. Nulla massa ante, vulputate at nisl in, lobortis faucibus risus. Curabitur in purus eros. Ut iaculis vitae enim quis sodales. Donec non faucibus mi. Sed tincidunt laoreet nulla, ut condimentum ligula malesuada sit amet. Fusce lobortis ullamcorper lectus, a mattis risus varius nec. Ut eu velit orci. Fusce vitae sodales ante. Donec consectetur, felis sit amet semper suscipit, sapien nunc accumsan nisi, vel laoreet purus ligula sit amet lectus. Etiam non odio condimentum, dignissim risus in, cursus dui. Nulla blandit molestie est. Duis vitae sagittis est. Ut condimentum dapibus nisi. Nullam vestibulum urna quis porttitor ullamcorper. Sed quis porttitor ipsum. Sed euismod sodales augue, et tincidunt ex tempor sit amet. Praesent scelerisque tellus et sapien venenatis, eget sodales leo mattis. Sed pretium arcu at nibh vulputate placerat.
+        This is a webapp for all fans of mathematics. You can carryout simple mathematical calculation using our inbuilt calculator. Also checkout the quote section for some fun facts on randomly chosen whole numbers. 
+        <br></br>
+        <br></br>
+        Do you know? 
+        From 0 to 1000, the only number that has the letter “a” in it is “one thousand”.
+        <br></br>
+        <br></br>
+        Checkout more cool facts....
+        <br></br>
+        <br></br>
+        <br></br>
+        This Single-Page-app was created using React
         </p>
       </div>
     </div>
