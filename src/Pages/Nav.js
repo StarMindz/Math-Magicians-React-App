d6dec43745cd96b5690564c9c9398a3bf2a49e80
StarMindz <64619873+StarMindz@users.noreply.github.com>
2022-11-16 16:15:40 +0100

    make webapp mobile responsive

--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -1,14 +1,27 @@
 /* eslint-disable arrow-body-style */
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Nav = () => {
+  const [ state, setState ] = useState({toggle: true});
+
+  const pullMenu = () => {
+    console.log("go girl")
+    setState((state) => {return {toggle: !state.toggle}});
+  }
+
   return (
     <div className="navBar">
-      <h1 className="title">Math Magician</h1>
-      <div className="menuItems">
-        <Link className="homeLink" to="/">HOME</Link>
-        <Link className="calculatorLink" to="/calculator">CALCULATOR</Link>
-        <Link className="quoteLink" to="/quote">QUOTES</Link>
+      <h1><a className="title" href='/#'> Math Magician </a></h1>
+      <div className={state.toggle ? 'menuItems':'menuItems active'}>
+        <Link onClick={pullMenu} className="homeLink" to="/">HOME</Link>
+        <Link onClick={pullMenu} className="calculatorLink" to="/calculator">CALCULATOR</Link>
+        <Link  onClick={pullMenu}className="quoteLink" to="/quote">QUOTES</Link>
+      </div>
+      <div className={state.toggle ? 'hamburger':'hamburger active'} onClick={pullMenu}>
+        <span className='horizontal'></span>
+        <span className='horizontal'></span>
+        <span className='horizontal'></span>
       </div>
     </div>
   );
